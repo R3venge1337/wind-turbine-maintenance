0395b9f4737ff5498697f7122f18d752164f6f66
Adam Zimny <adamczyk97@op.pl>
2026-01-01 18:44:31 +0100

    Initial commit: Spring Boot project structure with Kafka and Postgres

---
+++ backend/src/main/java/pl/zimnya/wind_turbine_maintenance/WindTurbineMaintenanceApplication.java
@@ -0,0 +1,13 @@
+package pl.zimnya.wind_turbine_maintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindTurbineMaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindTurbineMaintenanceApplication.class, args);
+	}
+
+}
