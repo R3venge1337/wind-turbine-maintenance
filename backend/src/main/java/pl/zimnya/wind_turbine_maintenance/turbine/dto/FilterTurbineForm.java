567553f186fb2e7d386a3173f60e33d4eacf4d51
Adam Zimny <adamczyk97@op.pl>
2026-01-14 00:46:06 +0100

    Add severity field to filterTurbineForm

--- backend/src/main/java/pl/zimnya/wind_turbine_maintenance/turbine/dto/FilterTurbineForm.java
+++ backend/src/main/java/pl/zimnya/wind_turbine_maintenance/turbine/dto/FilterTurbineForm.java
@@ -3,6 +3,7 @@ package pl.zimnya.wind_turbine_maintenance.turbine.dto;
 public record FilterTurbineForm(
         String productId,
         String typeCode,
+        String severity,
         String city,
         Integer minToolWear,
         Integer maxToolWear
