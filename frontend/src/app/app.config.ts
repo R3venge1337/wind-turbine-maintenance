c883600a927f827a8a1218b0014545430255c120
Adam Zimny <adamczyk97@op.pl>
2026-01-14 01:07:17 +0100

    Add translateService configuration

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,10 +2,27 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader, TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  // Zwracamy czystą instancję klasy - to omija błąd z brakującym InjectionTokenem
+  return new TranslateHttpLoader();
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTranslateService({
+      lang: 'pl',
+      fallbackLang: 'pl',
+      loader: provideTranslateHttpLoader({
+        prefix: 'assets/i18n/',
+        suffix: '.json'
+      })
+    }),
   ]
 };
