fceb9d797a8727356de69f0c60baf0fc579a11d5
Adam Zimny <adamczyk97@op.pl>
2026-01-14 01:08:08 +0100

    Add routes paths

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'map', pathMatch: 'full' },
+  {
+     path: 'map', loadComponent: () => import('./features/wind-turbine-map/components/wind-turbine-map/wind-turbine-map').then(m => m.WindTurbineMap)
+  },
+  {
+     path: 'turbines', loadComponent: () => import('./features/turbine/pages/turbine-table-view-page/turbine-table-view-page').then(m => m.TurbineTableViewPage)
+  },
+  {
+    path: 'turbines/:productId', loadComponent: () => import('./features/turbine/pages/turbine-detail-page/turbine-detail-page').then(m => m.TurbineDetailPage)
+  }
+];
